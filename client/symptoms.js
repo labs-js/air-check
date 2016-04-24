5343f4870a8c29e7c11c7afafe204b75f2793448
ricardogcolombo <ricardogcolombo@gmail.com>
2016-04-24 11:04:29 -0300

    Add back button

--- client/symptoms.js
+++ client/symptoms.js
@@ -1,23 +1,32 @@
-  $("#submitSymptoms").click(function() {
-      alert('changes submited');
-  });
-
-  $(".symptomButton").click(function() {
-      // get selected
-      $(".selected").removeClass("selected");
-      // select new element
-      $(this).addClass("selected");
-  });
+// select symptom
+$(".symptomButton").click(function() {
+    // get selected
+    $(".selected").removeClass("selected");
+    // select new element
+    $(this).addClass("selected");
+});
 
 
-
+// SLIDER 
 $(document).bind("pagecreate", function(event, ui) {
-    $('#slider').siblings('.ui-slider').bind('tap', function(event, ui){ makeAjaxChange($(this).siblings('input')); });
-    $('#slider').siblings('.ui-slider a').bind('taphold', function(event, ui){ makeAjaxChange($(this).parent().siblings('input')); 
+    $('#slider').siblings('.ui-slider').bind('tap', function(event, ui) {
+        makeAjaxChange($(this).siblings('input'));
+    });
+    $('#slider').siblings('.ui-slider a').bind('taphold', function(event, ui) {
+        makeAjaxChange($(this).parent().siblings('input'));
+    });
+
+    function makeAjaxChange(elem) {
+        alert(elem.val());
+    }
 });
 
-function makeAjaxChange( elem ) { 
-    alert(elem.val()); 
-}
+// save button
+$("#submitSymptoms").click(function() {
+    alert('changes submited');
 });
-  
+
+// Back button
+$("#backButton").click(function() {
+    alert("Volve!!!");
+});
